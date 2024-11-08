c2ecc8c0a6eea03690f530d562e8084db042bef0
abu <abusayed310278@gmail.com>
2024-11-08 20:17:44 +0600

    initial

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs=require('fs');
 
-
+/*
 //Blocking,synchronous way
 const input=fs.readFileSync('./input.txt','utf-8');
 console.log(input);
@@ -8,5 +8,10 @@ console.log(input);
 const output=`this is input file: ${input}.\n created at ${Date.now()}`
 
 fs.writeFileSync('output.txt',output);
+*/
 
+fs.readFile('input.txt','utf-8',(err,data)=>{
+    console.log(data)
+})
 
+console.log('will read this file');
