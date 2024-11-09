fedbad89b700e14dd6e8efdba0e9d42e5294eeee
abu <abusayed310278@gmail.com>
2024-11-10 01:07:37 +0600

    streams v2

--- streams.js
+++ streams.js
@@ -4,10 +4,30 @@ const server=require('http').createServer()
 server.on('request',(req,res)=>{
 
     //solution 1
-    fs.readFile('test-file.txt',(err,data)=>{
-        if(err)console.log(err)
-        res.end(data)
+    // fs.readFile('test-file.txt',(err,data)=>{
+    //     if(err)console.log(err)
+    //     res.end(data)
+    // })
+
+    //solution 2
+    const readable=fs.createReadStream('test-file.txt');
+
+    readable.on('data',chunk=>{
+        res.write(chunk)
     })
+
+    readable.on('end',()=>{
+        res.end()
+    })
+
+    readable.on('error',err=>{
+        console.log(err)
+        res.statusCode=500
+        res.end('file not found')
+    })
+    
+
+
 })
 
 
