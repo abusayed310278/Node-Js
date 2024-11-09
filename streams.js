f0eafd5cfd3edf8842f519052e2deb8d6b7fafff
abu <abusayed310278@gmail.com>
2024-11-10 01:14:24 +0600

    streams v3

--- streams.js
+++ streams.js
@@ -3,29 +3,39 @@ const server=require('http').createServer()
 
 server.on('request',(req,res)=>{
 
-    //solution 1
+    /*
+    //solution 1--load full file
     // fs.readFile('test-file.txt',(err,data)=>{
     //     if(err)console.log(err)
     //     res.end(data)
     // })
 
-    //solution 2
-    const readable=fs.createReadStream('test-file.txt');
+    */
 
-    readable.on('data',chunk=>{
-        res.write(chunk)
-    })
+    /*
+    //solution 2--back pressure
+    // const readable=fs.createReadStream('test-file.txt');
 
-    readable.on('end',()=>{
-        res.end()
-    })
+    // readable.on('data',chunk=>{
+    //     res.write(chunk)
+    // })
 
-    readable.on('error',err=>{
-        console.log(err)
-        res.statusCode=500
-        res.end('file not found')
-    })
+    // readable.on('end',()=>{
+    //     res.end()
+    // })
+
+    // readable.on('error',err=>{
+    //     console.log(err)
+    //     res.statusCode=500
+    //     res.end('file not found')
+    // })
+
+    */
     
+    /* */
+    //solution 3--solve this backpressure of solution-2 code
+    const readable=fs.createReadStream('test-file.txt');
+    readable.pipe(res);
 
 
 })
