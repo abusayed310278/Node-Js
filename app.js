81e81446f40d4e37e8349bed30a08868f70ef690
abu <abusayed310278@gmail.com>
2024-11-10 14:07:13 +0600

    post request

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express=require('express')
+const fs=require('fs');
+const express=require('express');
+const app=express();
 
-const app=express()
 
-app.get('/',(req,res)=>{
-    // res.status(200).send('hello from the server')
-    res.status(200).json({message:'hello from the server'})
+//middleware b/w req and res
+app.use(express.json())
+
+
+const tours=JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{ tours }
+        
+    })
 })
 
-app.post('/',(req,res)=>{
-    res.send('you can post to this endpoint......')
+app.post('/api/v1/tours',(req,res)=>{
+    console.log(req.body)
+    res.send('done');
 })
+
+
 
 const port=3000
 app.listen(port,()=>{
-    console.log(`app running on port${port}....`)
+    console.log(`app running on port at ${port}....`)
 })
