ffe57955c7c52f04fc28f954e9cc17e278effb41
abu <abusayed310278@gmail.com>
2024-11-12 20:58:15 +0600

    vertual property

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,60 +1,68 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const tourSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'A tour must have a name'],
-        unique:true,
-        trim:true
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+        trim: true
     },
-    duration:{
-      type:Number,
-      required:[true,'A tour must have a duration']
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
     },
-    maxGroupSize:{
-      type:Number,
-      required:[true,'A tour must have a group size']
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
     },
-    difficulty:{
-      type:String,
-      required:[true,'A tour must have difficulty']
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have difficulty']
     },
-    rating:{
-        type:Number,
-        default:4.5,
+    rating: {
+        type: Number,
+        default: 4.5,
     },
-    price:{
-        type:Number,
-        required:[true,'A tour must have a price']
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
     },
-    priceDiscount:Number,
-    summary:{
-        type:String,
-        trim:true,
-        required:[true, 'A tour must have a description']
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
     },
-    description:{
-        type:String,
-        trim:true
+    description: {
+        type: String,
+        trim: true
     },
-    imageCover:{
-        type:String,
-        required:[true,'A tour must have a cover image']
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
     },
-    images:[String],
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-        select:false
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
     },
-    startDates:[Date],
+    startDates: [Date],
 
+},
+    {
+        toJSON: {virtuals: true},
+        toObject: {virtuals: true},
+    }
+);
 
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7
+})
 
-});
 
 //here Tour is model madel name so its named as Tour which is convention of node.js
-const Tour=mongoose.model('Tour',tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
 
 
-module.exports=Tour;
+module.exports = Tour;
