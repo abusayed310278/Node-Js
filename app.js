b44e6c795a30027d354ec0ac56b1f28d5916d3f4
abu <abusayed310278@gmail.com>
2024-11-18 12:00:18 +0600

    create user model

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const AppError=require('./utils/appError')
 const globalErrorHandler=require('./controllers/errorController')
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const rateLimit = require("express-rate-limit");
+const helmet=require('helmet')
+const xss=require('xss-clean')
+const mongoSanitize=require('express-mongo-sanitize')
+const hpp = require('hpp');
 
 const app = express();
 
 /*global middleware */
+// Set security HTTP headers
+app.use(helmet())
 if (process.env.NODE_ENV === 'development') {
 
     app.use(morgan("dev"));
@@ -27,6 +32,28 @@ app.use('/api',limiter)
 
 //middleware b/w req and res
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit:'10kb'}))
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist:[
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}))
+
 
 //use route for middleware for rendering static file
 app.use(express.static(`${__dirname}/public`))
