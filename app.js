62cd48c245355467943dea71b56032ac6776d630
abu <abusayed310278@gmail.com>
2024-11-20 09:23:44 +0600

    pug setting:

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path=require('path')
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,15 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+
 // 1) GLOBAL MIDDLEWARES
+//Serving static files
+app.use(express.static(path.join(__dirname,'public')))
+
+
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -55,7 +64,8 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+
 
 // Test middleware
 app.use((req, res, next) => {
@@ -65,6 +75,11 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/',(req,res)=>{
+  res.status(200).render('base')
+})
+
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
