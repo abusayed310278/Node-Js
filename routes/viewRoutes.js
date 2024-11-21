a71506c907662647f0f064cae7ee46c03b5d4620
abu <abusayed310278@gmail.com>
2024-11-21 07:23:51 +0600

    mapbox

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,19 +1,20 @@
 const express=require('express')
 const viewsController=require('../controllers/viewsController')
+const authController = require('../controllers/authController');
 
 const router=express.Router()
 
-router.get('/',(req,res)=>{
-    res.status(200).render('base',{
-        tour:'the forest hiker',
-        user:'jones'
-    })
-})
+// router.get('/',(req,res)=>{
+//     res.status(200).render('base',{
+//         tour:'the forest hiker',
+//         user:'jones'
+//     })
+// })
 
 
 router.get('/',viewsController.getOverview)
-
-router.get('/tour',viewsController.getTour)
+router.get('/tour/:slug',viewsController.getTour)
+router.get('/login',viewsController.getLoginForm);
 
 
 
